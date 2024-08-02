9509e63b7615ec8f3fbb596210f82a15bda77b65
Jerome Bonhomme <bonhommej@gmail.com>
2024-08-01 17:49:16 -0700

    main image

--- frontend/src/components/AuthAmplify.tsx
+++ frontend/src/components/AuthAmplify.tsx
@@ -24,7 +24,7 @@ const AuthAmplify: React.FC<Props> = ({ socialProviders, children }) => {
         Header: () => (
           <div>
             <div className='Image' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-              <img src="/public/images/tokkai.png" alt="Tokkai Image" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+              <img src="/images/tokkai.png" alt="Tokkai Image" style={{ maxWidth: '100%', maxHeight: '100%' }} />
             </div>
 
             <div className="mb-5 mt-10 flex justify-center text-3xl text-aws-font-color">
