66c1141262bf5f691279655c4fbe74751960405b
Jerome Bonhomme <bonhommej@gmail.com>
2024-08-09 08:35:37 -0700

    Testing Py 3

--- frontend/src/components/AudioStreamer.tsx
+++ frontend/src/components/AudioStreamer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 // import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 // import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 // import { Auth } from 'aws-amplify';
